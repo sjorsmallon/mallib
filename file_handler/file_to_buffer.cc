3ded42e3e4dedb01580ad5e48c833341ab3949c2
SJMdev <sjorsmallon@gmail.com>
2019-06-15 22:48:57 +0200

    file_to_buffer is broken

--- file_handler/file_to_buffer.cc
+++ file_handler/file_to_buffer.cc
@@ -17,8 +17,9 @@ bool file::file_to_buffer(const string& filename, char *buffer)
                        (std::istreambuf_iterator<char>()    ) );
 
     //@Leak: this is a copy with new, as there is no destructor here.
-    buffer = new char[s0.size() + 1];
-    memcpy(s4, s0.c_str(), s0.size() + 1);
+    //@Leak: this is not correct?
+    buffer = new char[content.size() + 1];
+    memcpy(buffer, content.c_str(), content.size() + 1);
 
 
 }
