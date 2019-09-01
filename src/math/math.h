4127aa56bca63a9bfe8b9d27e2a965c9d16baa15
SJMdev <sjorsmallon@gmail.com>
2019-09-01 20:20:21 +0200

    math: refactored to namespace

--- src/math/math.h
+++ src/math/math.h
@@ -1,31 +1,22 @@
 #ifndef INCLUDED_MATH_
 #define INCLUDED_MATH_
 #include <cmath>
-// #define IEEE_FLT_MANTISSA_BITS  23
 
-
-class Math
+namespace math
 {
-    public:
-        static void Init();
+    static float invSqrt(const float x);
 
-        static float invSqrt(const float x);
+    static inline const float PI = 3.14159265358979323846f;
 
-        static inline const float PI = 3.14159265358979323846f;
+    static inline const float INF = 1e30f;
 
-        static inline const float INF = 1e30f;
+    static inline const float DEG2RAD = PI / 180.0f;
 
-        static inline const float DEG2RAD = PI / 180.0f;
+    static inline const float RAD2DEG = 180.f / PI;
 
-        static inline const float RAD2DEG = 180.f / PI;
+    static inline const float SEC2MS = 1000.0f;
 
-        static inline const float SEC2MS = 1000.0f;
-
-        static inline const float MS2SEC = 0.001f;
-
-
-        // static inline const float FLT_SMALLEST_NON_DENORMAL;
-
+    static inline const float MS2SEC = 0.001f;
 };
 
 inline float Math::invSqrt(const float x)
