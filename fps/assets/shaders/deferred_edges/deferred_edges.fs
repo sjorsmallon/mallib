d108292d1889b824b6a808e5492454f3cdb98399
Work <sjorsmallon@gmail.com>
2020-10-27 19:33:31 +0100

    [feat] edge shader

--- fps/assets/shaders/deferred_edges/deferred_edges.fs
+++ fps/assets/shaders/deferred_edges/deferred_edges.fs
@@ -34,19 +34,46 @@ const float offY = 1.0 / WINDOW_HEIGHT;
 
 const float threadHold = 0.10;
 
-// color sobel
-bool sobel_color(vec2 texture_coords)
-{
-    float s00 = texture(g_color, texture_coords + vec2(-offX, -offY)).a;
-    float s01 = texture(g_color, texture_coords + vec2( 0,   -offY)).a;
-    float s02 = texture(g_color, texture_coords + vec2( offX, -offY)).a;
 
-    float s10 = texture(g_color, texture_coords + vec2(-offX,  0)).a;
-    float s12 = texture(g_color, texture_coords + vec2( offX,  0)).a;
+bool sobel_albedo(vec2 texture_coords)
+{
+    vec3 c11 = texture(g_albedo_spec, texture_coords).xyz;
+   
+    float  s00 = max(0.0f, dot(c11, texture(g_albedo_spec, texture_coords + vec2(-offX, -offY)).xyz) - threadHold);   
+    float  s01 = max(0.0f, dot(c11, texture(g_albedo_spec, texture_coords + vec2(   0.0f, -offY)).xyz)  - threadHold);
+    float  s02 = max(0.0f, dot(c11, texture(g_albedo_spec, texture_coords + vec2( offX, -offY)).xyz) - threadHold);
+
+    float  s10 = max(0.0f, dot(c11, texture(g_albedo_spec, texture_coords + vec2(-offX,   0.0f)).xyz) - threadHold);
+    float  s12 = max(0.0f, dot(c11, texture(g_albedo_spec, texture_coords + vec2( offX,   0.0f)).xyz) - threadHold);
+   
+    float  s20 = max(0.0f, dot(c11, texture(g_albedo_spec, texture_coords + vec2(-offX, offY)).xyz) - threadHold);
+    float  s21 = max(0.0f, dot(c11, texture(g_albedo_spec, texture_coords + vec2(   0.0f,  offY)).xyz) - threadHold);
+    float  s22 = max(0.0f, dot(c11, texture(g_albedo_spec, texture_coords + vec2( offX, offY)).xyz) - threadHold);
+   
+    float sobelX = s00 + 2.0f * s10 + s20 - s02 - 2.0f * s12 - s22;
+    float sobelY = s00 + 2.0f * s01 + s02 - s20 - 2.0f * s21 - s22;
+
+    float edgeSqr = (sobelX * sobelX + sobelY * sobelY);
+
+    bool edge_over_treshold = edgeSqr > (0.07 * 0.07);
+
+    return edge_over_treshold;
+}
+
+
+// specular sobel
+bool sobel_specular(vec2 texture_coords)
+{
+    float s00 = texture(g_albedo_spec, texture_coords + vec2(-offX, -offY)).a;
+    float s01 = texture(g_albedo_spec, texture_coords + vec2( 0,   -offY)).a;
+    float s02 = texture(g_albedo_spec, texture_coords + vec2( offX, -offY)).a;
+
+    float s10 = texture(g_albedo_spec, texture_coords + vec2(-offX,  0)).a;
+    float s12 = texture(g_albedo_spec, texture_coords + vec2( offX,  0)).a;
     
-    float s20 = texture(g_color, texture_coords + vec2(-offX,  offY)).a;
-    float s21 = texture(g_color, texture_coords + vec2( 0,    offY)).a;
-    float s22 = texture(g_color, texture_coords + vec2( offX,  offY)).a;
+    float s20 = texture(g_albedo_spec, texture_coords + vec2(-offX,  offY)).a;
+    float s21 = texture(g_albedo_spec, texture_coords + vec2( 0,    offY)).a;
+    float s22 = texture(g_albedo_spec, texture_coords + vec2( offX,  offY)).a;
     
     float sobelX = s00 + 2.0f * s10 + s20 - s02 - 2.0f * s12 - s22;
     float sobelY = s00 + 2.0f * s01 + s02 - s20 - 2.0f * s21 - s22;
@@ -56,14 +83,14 @@ bool sobel_color(vec2 texture_coords)
     bool edge_over_treshold = edgeSqr > (0.07 * 0.07);
 
 
-    return 1.0f - (edgeSqr > 0.07 * 0.07);
+    return false;
 }
 
 
 bool sobel_normal(vec2 texture_coords)
 {
 
-    float3 c11 = texture(g_normal, texture_coords).xyz;
+    vec3 c11 = texture(g_normal, texture_coords).xyz;
    
     float  s00 = max(0.0f, dot(c11, texture(g_normal, texture_coords + vec2(-offX, -offY)).xyz) - threadHold);   
     float  s01 = max(0.0f, dot(c11, texture(g_normal, texture_coords + vec2(   0.0f, -offY)).xyz)  - threadHold);
@@ -81,9 +108,9 @@ bool sobel_normal(vec2 texture_coords)
 
     float edgeSqr = (sobelX * sobelX + sobelY * sobelY);
 
+    bool edge_over_treshold = edgeSqr > (0.07 * 0.07);
 
-
-    return 1.0f - (edgeSqr > 0.07 * 0.07);
+    return edge_over_treshold;
 }
 
 
@@ -91,17 +118,29 @@ void main()
 {             
     // retrieve data from G-buffer
     vec3 fragment_position = texture(g_position, texture_coords).rgb;
-
     vec3 normal = texture(g_normal, texture_coords).rgb;
     vec3 albedo = texture(g_albedo_spec, texture_coords).rgb;
     float specular = texture(g_albedo_spec, texture_coords).a;
 
-    bool sc = sobel_color(texture_coords);
-    bool sn = sobel_normal(texture_coords); 
+    bool salbedo = sobel_albedo(texture_coords);
+    bool snormal = sobel_normal(texture_coords); 
+    bool sspecular = sobel_specular(texture_coords); 
 
 
+    vec3 sobel_value = vec3(0.0f);
 
-    fragment_color = vec4(vec3(sobel_value), 1.0);
+    // if (salbedo || snormal || sspecular)
+    // {
+    //     sobel_value = vec3(1.0f);
+    // }
+
+    if (snormal)
+    {
+        sobel_value = vec3(1.0f);
+    }
+
+
+    fragment_color = vec4(sobel_value, 1.0);
 }  
 
 
