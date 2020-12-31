993046e2549a23deddffc30e2a966859b6a60627
Sjors <sjorsmallon@gmail.com>
2020-12-31 16:14:11 +0100

    [feat] more movement. airborn fwd & backward results in NaN

--- fps/src/game_simulate.cc
+++ fps/src/game_simulate.cc
@@ -65,17 +65,19 @@ namespace
     float pm_friction = 6.f;
     float pm_stopspeed = 100.0f;
     float pm_maxvelocity = 320.0f;
+
+
+   	float g_gravity = 800.0f;
     float pm_jumpvelocity = 270.0f;
 
    	float input_axial_extreme = 127.0f; // -127, 127 for forward and backward (for controller support)
 
-    float pm_jump_acceleration  = 35.f;
-
     float g_mouse_sensitivity = 0.08f;
     float g_camera_velocity = 0.2f;
- 	float g_player_gravity = 50.f;
 
- 	float CLIP_OVERCLIP = 1.0001f;
+
+
+ 	float CLIP_OVERCLIP = 1.001f;
     
     
 
@@ -97,6 +99,7 @@ namespace
 
 		float velocity = glm::length(old_movement_vector);
 
+		//@Cleanup(Sjors): fix the velocity flooring threshold.
 		const float velocity_treshold = 0.1f;
 		if (velocity < velocity_treshold)
 		{
@@ -105,42 +108,44 @@ namespace
 		}
 
 		float velocity_drop = 0.0f;
-		// disregard jump for now.
+
 		if (grounded && !jump_pressed_this_frame)
 		{
 			float control = velocity < pm_stopspeed ? pm_stopspeed : velocity;
-			logr::report_warning("applying friction!\n");
 			velocity_drop += control * pm_friction * dt;
 		}
 
 		// adjust the velocity with the induced velocity drop. 
 		float adjusted_velocity = velocity - velocity_drop;
 
+		// cannot move in the negatives.
 		if(adjusted_velocity < 0.0f) adjusted_velocity = 0.0f;
 
 		// normalize the velocity based on the ground velocity.
-		if(adjusted_velocity > 0.0f)
-		{
-			adjusted_velocity /= velocity;
-		}
+		if(adjusted_velocity > 0.0f) adjusted_velocity /= velocity;
 
-		//@IC(Sjors): we don't need to normalize below since we do that above (normalize the new_velocity by velocity.)
+		//@IC(Sjors): we don't need to the movement vector below since we do that above (normalize the new_velocity by velocity)
 		return old_movement_vector * adjusted_velocity;
 	}
 
+	// since input can be provided -127 -> +127, we scale the movement vector based on the input delivered.
     [[nodiscard]]
-    float apply_scale(const float forward_move,const float right_move, const float up_move, const float speed)
+    float calculate_input_scale(const float forward_move,const float right_move, const float up_move, const float max_velocity)
     {
-    	int max = abs(forward_move);
-    	if (abs(right_move) > max) max = abs(right_move);
+    	//@Cleanup(Sjors): get rid of these casts.
+    	int max = abs(static_cast<int>(forward_move));
+    	if (abs(static_cast<int>(right_move)) > max) max = abs(static_cast<int>(right_move));
+
+    	if (abs(static_cast<int>(up_move)) > max) max = abs(static_cast<int>(up_move));
 
     	if (!max) return 0.f;
 
-    	float total = sqrt(forward_move * forward_move + right_move * right_move);
+    	float total = sqrt(forward_move * forward_move + right_move * right_move + up_move * up_move);
     	float scale = pm_maxvelocity * static_cast<float>(max) / (input_axial_extreme * total);
     	return scale;
     }
 
+    [[nodiscard]]
     glm::vec3 clip_velocity(glm::vec3 in, glm::vec3 normal, const float overbounce)
     {
     	float backoff = glm::dot(in, normal);
@@ -159,13 +164,11 @@ namespace
 
     	return result;
     }
-  
-	
+
+	//@TODO(Sjors) NaN checks etc.
 	[[nodiscard]]
 	glm::vec3 accelerate(glm::vec3 adjusted_movement_vector, glm::vec3 wish_direction, float wish_velocity, float acceleration, float dt)
 	{
-		logr::report("[accelerate]\n");
-
 		float current_speed_in_wish_dir = glm::dot(adjusted_movement_vector, wish_direction);
 		float add_speed = wish_velocity - current_speed_in_wish_dir;
 
@@ -176,7 +179,7 @@ namespace
 
 		glm::vec3 result = adjusted_movement_vector + acceleration_speed * wish_direction;
 
-		// //@IC(Sjors): NaN check.
+		// //@FIXME(Sjors): NaN check.
 		// if ((current_speed_in_wish_dir != current_speed_in_wish_dir) && adjusted_velocity > 0.0f)
 		// {
 		// 	logr::report_warning("opposite directions pressed..\n");
@@ -193,10 +196,46 @@ namespace
 		return result;
 	}	
 
-    //@Dependencies:
-    // g_player_acceleration
-    // world_up
-	std::tuple<glm::vec3, glm::vec3> update_position_with_input(
+	// if we succeeded and did not clip it, ....
+	// glm::vec3 slide_move(const glm::vec3 old_position, const glm::vec3 movement_vector, const bool player_is_airborne, const float dt)
+	// {
+	// 	if (player_is_airborne)
+	// 	{
+	// 		glm::vec3 new_movement_vector = movement_vector;
+	// 		new_movement_vector.y -= g_gravity * dt;
+	// 	}
+
+	// 	glm::vec3 position = old_position + (new_movement_vector * dt);
+	// 	return position;
+	// }
+
+	// ah, I guess this is to check for incremental changes in height.
+	//@Incomplete: check against geometry?
+	// dependencies:
+	// g_gravity
+	[[nodiscard]]
+	std::tuple<glm::vec3, glm::vec3> step_slide_move(const glm::vec3 old_position, const glm::vec3 movement_vector, const bool player_is_airborne, const float dt)
+	{
+		glm::vec3 new_movement_vector = movement_vector;
+		if (player_is_airborne)
+		{
+			logr::report("player_is_airborne: {}\n", player_is_airborne);
+			new_movement_vector.y -= g_gravity * dt;
+			logr::report("new_movement_vector.y: {}\n", new_movement_vector.y);
+		}
+		glm::vec3 position = old_position + (new_movement_vector * dt);
+		return std::make_tuple(position, new_movement_vector);
+	}
+
+	[[nodiscard]]
+	bool check_jump(const Input& input)
+	{
+		// conditions that deal with jumping.
+		// if held, wait for release. Otherwise, if on the ground & ...
+		return input.keyboard_state[KEY_SPACE];
+	}
+
+	std::tuple<glm::vec3, glm::vec3> air_move(
 		const Input& input,
 		const glm::vec3 old_position,
 		const glm::vec3 old_movement_vector,
@@ -204,31 +243,26 @@ namespace
 		const glm::vec3 right,
 		const float dt)
 	{
-		static bool grounded = true;
-		static float hang_time = 0.0f;
-		bool jump_pressed_this_frame = input.keyboard_state[KEY_SPACE];	
-
+		logr::report_warning("[air_move]\n");
+		bool grounded = false;
+		bool jump_pressed_this_frame = false;
+		
 		glm::vec3 adjusted_movement_vector = apply_friction(old_movement_vector, grounded, jump_pressed_this_frame, dt);
-		
-		//
-		adjusted_movement_vector.y = 0.0f;
-		float Y_old_y = old_movement_vector.y;
-		//
-
 		float forward_input = input_axial_extreme * input.keyboard_state[KEY_W] - input_axial_extreme * input.keyboard_state[KEY_S];
-		float right_input = input_axial_extreme * input.keyboard_state[KEY_D] - input_axial_extreme * input.keyboard_state[KEY_A];
-		
-		float scale = apply_scale(forward_input, right_input, 0.0f, pm_maxvelocity);
+		float right_input   = input_axial_extreme * input.keyboard_state[KEY_D] - input_axial_extreme * input.keyboard_state[KEY_A];
+		float up_input      = input_axial_extreme;
+		float scale = calculate_input_scale(forward_input, right_input, up_input, pm_maxvelocity);
 
-		glm::vec3 plane_front = clip_velocity(front, glm::vec3(0.0f,1.0f,0.0f), CLIP_OVERCLIP);
-		glm::vec3 plane_right = clip_velocity(right, glm::vec3(0.0f,1.0f,0.0f), CLIP_OVERCLIP);
-
+		glm::vec3 plane_front = glm::vec3(front.x, 0.0f, front.z);
+		glm::vec3 plane_right = glm::vec3(right.x, 0.0f, right.z);
+		plane_front = clip_velocity(plane_front, glm::vec3(0.0f,1.0f,0.0f), CLIP_OVERCLIP);
+		plane_right = clip_velocity(plane_right, glm::vec3(0.0f,1.0f,0.0f), CLIP_OVERCLIP);
 		plane_front = glm::normalize(plane_front);
 		plane_right = glm::normalize(plane_right);
 
 		glm::vec3 wish_direction = plane_front * forward_input + plane_right * right_input;
 		float wish_velocity = 0.0f;
-
+		//@Incomplete(Sjors): is there a difference between generating new input & just finalizing?
 		bool received_input = (input.keyboard_state[KEY_W] ||
 							   input.keyboard_state[KEY_S] ||
 							   input.keyboard_state[KEY_A] ||
@@ -241,17 +275,70 @@ namespace
 			wish_direction = glm::normalize(wish_direction);
 		} 
 
-		float Y_input_y_velocity = 0.0f;
-		if (input.keyboard_state[KEY_SPACE])
+		// not on ground!
+		glm::vec3 movement_vector = accelerate(old_movement_vector, wish_direction, wish_velocity, pm_air_acceleration, dt);
+
+		bool player_is_airborne = true;
+	
+		return step_slide_move(old_position, movement_vector, player_is_airborne, dt);
+	}
+    //@Dependencies:
+    // g_player_acceleration
+    // world_up
+	std::tuple<glm::vec3, glm::vec3> walk_move(
+		const Input& input,
+		const glm::vec3 old_position,
+		const glm::vec3 old_movement_vector,
+		const glm::vec3 front,
+		const glm::vec3 right,
+		const float dt)
+	{
+
+		static bool grounded = true;
+		bool jump_pressed_this_frame = check_jump(input);
+
+		if (jump_pressed_this_frame && grounded)
 		{
-			if (grounded)
-			{
-				hang_time = 0.0f;
-				grounded = false;
-			}
+			grounded = false;
+			glm::vec3 jump_movement_vector = old_movement_vector;
+			jump_movement_vector.y = pm_jumpvelocity;
+			return air_move(input, old_position, jump_movement_vector, front, right, dt);	
 		}
 
+		// otherwise, we wouldn't be in here!
+		grounded = true;
+		// apply friction
+		glm::vec3 adjusted_movement_vector = apply_friction(old_movement_vector, grounded, jump_pressed_this_frame, dt);
+		float forward_input = input_axial_extreme * input.keyboard_state[KEY_W] - input_axial_extreme * input.keyboard_state[KEY_S];
+		float right_input   = input_axial_extreme * input.keyboard_state[KEY_D] - input_axial_extreme * input.keyboard_state[KEY_A];
+		float up_input      = input_axial_extreme * (jump_pressed_this_frame && grounded);
+		float scale = calculate_input_scale(forward_input, right_input, up_input, pm_maxvelocity);
 
+		glm::vec3 plane_front = glm::vec3(front.x, 0.0f, front.z);
+		glm::vec3 plane_right = glm::vec3(right.x, 0.0f, right.z);
+		plane_front = clip_velocity(plane_front, glm::vec3(0.0f,1.0f,0.0f), CLIP_OVERCLIP);
+		plane_right = clip_velocity(plane_right, glm::vec3(0.0f,1.0f,0.0f), CLIP_OVERCLIP);
+		plane_front = glm::normalize(plane_front);
+		plane_right = glm::normalize(plane_right);
+
+		glm::vec3 wish_direction = plane_front * forward_input + plane_right * right_input;
+		float wish_velocity = 0.0f;
+
+		//@Incomplete(Sjors): is there a difference between generating new input & just finalizing?
+		bool received_input = (input.keyboard_state[KEY_W] ||
+							   input.keyboard_state[KEY_S] ||
+							   input.keyboard_state[KEY_A] ||
+							   input.keyboard_state[KEY_D]);
+
+		// normalize input vector, but keep velocity! this HAS to be normalized by dt.
+		if (received_input)
+		{
+			wish_velocity  = scale * glm::length(wish_direction);
+			wish_direction = glm::normalize(wish_direction);
+		} 
+		// if we are ducking
+		// if (player_state & PMF_DUCKED) wish_speed = pm_maxspeed * pm_duckscale (pm_duckscale = 0.5)
+		
 		glm::vec3 movement_vector = glm::vec3(0.0f);
 
 		if (wish_velocity < 0.1f) 
@@ -262,49 +349,38 @@ namespace
 		}
 		else
 		{
-			// Take into account whether we are grounded or not.
+			// Take into account whether we need air value or not.
 			float acceleration = (grounded) ? pm_ground_acceleration : pm_air_acceleration;
 			movement_vector = accelerate(adjusted_movement_vector, wish_direction, wish_velocity, acceleration, dt);
 		}
 
 		// slide along the ground plane.
-		// clip the movement vector!
-		// float velocity = glm::length(movement_vector);
-		// clip_velocity(movement_vector, glm::vec3(0.0f,1.0f,0.0f), CLIP_OVERCLIP);
-		// movement_vector = velocity * movement_vector;
+		// clip the movement vector to not move into the ground.
+		float velocity  = glm::length(movement_vector);
+		movement_vector = clip_velocity(movement_vector, glm::vec3(0.0f,1.0f,0.0f), CLIP_OVERCLIP);
 
-		//Y
+		//@FIXME(Sjors): formalize this threshold.
+		float movement_treshold = 0.05f;
+
+		//@TEMPORARY!!!!!! the normalize below causes NaN if movement_vector is zero.
+		if (fabs(movement_vector.x) < movement_treshold && fabs(movement_vector.z) < movement_treshold)
 		{
-			movement_vector.y  = Y_old_y;
-			movement_vector.y += Y_input_y_velocity;
-
-			if (!grounded)
-			{
-				hang_time += dt;
-				float x = hang_time;
-				float c = 0.0f;
-				float b = pm_jump_acceleration;
-				float a = -g_player_gravity;
-				movement_vector.y = a * x * x + b * x + c; 
-			}
+			return std::make_tuple(old_position, glm::vec3(0.0f, movement_vector.y, 0.0f));
 		}
-		
 
-		// float movement_vector_velocity = glm::length(movement_vector); 
+		// don't decrease velocity when going down a slope.
+		movement_vector = glm::normalize(movement_vector);
+		movement_vector = velocity * movement_vector;
 
-		
-		//@NOTE: WE NEED TO MOVE _WITH_ DT(!!!!!!!!!!!!!)
-		glm::vec3 position = glm::vec3(old_position.x, 0.0f, old_position.z) + movement_vector * dt;
-		// // clip the movement vector.
-		if (position.y < 0.0f)
+		// skip checking the movement vector if the movement vector is too small.
+		if (fabs(movement_vector.x) < movement_treshold && fabs(movement_vector.z) < movement_treshold)
 		{
-			grounded = true;
-			position.y = 0.0f;
-			movement_vector.y = 0.0f;
-			hang_time = 0.0f;
+			return std::make_tuple(old_position, glm::vec3(0.0f, movement_vector.y, 0.0f));
 		}
-		
-		return std::make_tuple(position, movement_vector);
+
+		bool player_is_airborne = false;
+
+		return step_slide_move(old_position, movement_vector, player_is_airborne, dt);
 	}
 
     // @dependencies:
@@ -370,10 +446,20 @@ namespace
 	Camera update_camera_entity(const Input& input, const Camera old_camera, const float dt)
 	{
 		Camera camera = old_camera;
-		auto [position, movement_vector] = update_position_with_input(input, old_camera.position, old_camera.movement_vector, old_camera.front, old_camera.right, dt); 
-		camera.position = position;
-		camera.movement_vector = movement_vector;
 
+		if (old_camera.position.y  > 0.0f)
+		{
+			auto [position, movement_vector] = air_move(input, old_camera.position, old_camera.movement_vector, old_camera.front, old_camera.right, dt);
+			camera.position = position;
+			camera.movement_vector = movement_vector;
+		}
+		else
+		{
+			auto [position, movement_vector] = walk_move(input, old_camera.position, old_camera.movement_vector, old_camera.front, old_camera.right, dt); 
+			camera.position = position;
+			camera.movement_vector = movement_vector;
+		}
+	
 		if (input.mouse_delta_x || input.mouse_delta_y)
 			return update_camera_view_with_input(input, camera, dt);
 
@@ -490,6 +576,7 @@ void game_simulate(Game_State& game_state, const double dt, const Input& input, 
 		}
 			// update player and camera.
 			{
+
 				game_state.camera = update_camera_entity(input, game_state.camera, clamped_dt);
 				g_player_entity.position = game_state.camera.position;
 			}
